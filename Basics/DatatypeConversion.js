0be411f23e40fbc6b057314424732b02342fdff1
Abhay-333 <140523711+Abhay-333@users.noreply.github.com>
2024-06-01 14:28:43 +0000

    Comparisons

--- Basics/DatatypeConversion.js
+++ Basics/DatatypeConversion.js
@@ -22,3 +22,42 @@ console.log(Boolean(isLoggedIn))
 // "" => Boolean(false)
 // "string" => Boolean(true)
 // 0 => Boolean(false)
+
+// ******OPERATIONS*************
+
+console.log(2+"5");
+console.log(2+"5" + (4 + 4));
+console.log("2"+ 5 + 4 - 4);
+console.log( "5" + 4 - 4);
+
+let c = 9
+const d = c++
+
+let e = 19
+const f = ++e
+
+console.log(`c: ${c}, d: ${d}`);
+
+console.log(`c: ${e}, d: ${f}`);
+
+
+// ********COMPARISONS*************
+
+// console.log(4 === 2)
+// console.log(4 == 2)
+// console.log(25 !== 2)
+// console.log(12 <= 2)
+// console.log(5 >= 2)
+// console.log(22 < 2)
+// console.log(25 > 2)
+
+
+// The problem here is that null values are treated like a number here thats the output is confusing same goes with undefined values
+console.log(null == 0)
+console.log(null > 0)
+console.log(null >= 0)
+
+// The only difference is these 3 values output is false
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined >= 0)
---
+++ Basics/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+ole.log(4 === 2)
+// console.log(4 == 2)
+// console.log(25 !== 2)
+// console.log(12 <= 2)
+// console.log(5 >= 2)
+// console.log(22 < 2)
+// console.log(25 > 2)
